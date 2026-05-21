c4c40f6bdc5d15d4c5ce0174063cfe6d46012b1a
Fernando <f.miranda25d@gmail.com>
2026-05-21 05:47:37 -0500

    Third commit, uses single static scanner and less print statements

--- src/main/java/com/pluralsight/Program.java
+++ src/main/java/com/pluralsight/Program.java
@@ -3,23 +3,23 @@ package com.pluralsight;
 import java.util.Scanner;
 
 public class Program {
+    static Scanner input = new Scanner(System.in);
+
     public static void main(String[] args) {
 
         double sandwichPrice = 0;
 
-        Scanner userInput1 = new Scanner(System.in);
         System.out.println("Would you like sandwich size 1 or 2?: ");
         System.out.println("1: Regular: base price $5.45");
         System.out.println("2: Large: base price $8.95");
-        int sandwichSize = userInput1.nextInt();
+        int sandwichSize = input.nextInt();
+        input.nextLine();
 
-        Scanner userInput2 = new Scanner(System.in);
         System.out.println("Would you like your sandwich loaded?: ");
-        String loadPreference = userInput2.nextLine();
+        String loadPreference = input.nextLine();
 
-        Scanner userInput3 = new Scanner(System.in);
         System.out.println("Enter your age: ");
-        int userAge = userInput3.nextInt();
+        int userAge = input.nextInt();
 
         if (sandwichSize == 1){
             sandwichPrice = 5.45;
@@ -35,12 +35,9 @@ public class Program {
 
         if (userAge <= 17){
             sandwichPrice *= 0.9;
-            System.out.println(String.format("%.2f", sandwichPrice));
         } else if (userAge >= 65){
             sandwichPrice *= 0.8;
-            System.out.println(String.format("%.2f", sandwichPrice));
-        } else {
-            System.out.println(String.format("%.2f", sandwichPrice));
         }
+        System.out.printf("That will be $%.2f", sandwichPrice);
     }
 }
